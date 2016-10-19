189088efad84e94ed6a47027ee48350cfccecf74
ArchieKe <gertrudeochieng@gmail.com>
2016-10-19 14:37:35 +0300

    all functions working. Fixed while loop for wrong input prompt.

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import sys
 import os
 import json
 import shutil
+import time
 
 def quiz_list():
   """Shows all available quizzes in the global library."""
   path = "Global_library"
   quizzes = os.listdir( path )
-
-# This would print all the files and directories
   for file in quizzes:
     fileExt = os.path.splitext(file)[-1]
     if fileExt == '.json':
       print file[:-5]
+
 
 def myquiz_list():
   """ Displays a list of quizzes that the user has imported from the global library"""
   path = "My_library"
   quizzes = os.listdir( path )
-
-# This would print all the files and directories
   for file in quizzes:
     fileExt = os.path.splitext(file)[-1]
     if fileExt == '.json':
       print file[:-5]
+
 
 def quiz_import(quiz_name):
   quiz_path = 'Global_library'
@@ -33,27 +32,46 @@ def quiz_import(quiz_name):
     The quiz has been copied to your library. 
     Have fun with it!!"""
   
+
 def quiz_take(quiz_name):
   json_data = open("My_library" + "/" + quiz_name + ".json").read()
 
   counter = 0
+  score = 0
   current_quiz =  json.loads(json_data)
- 
+  start_time = time.time()
+  time_allocated = time.time() + 15
+
+  all_questions = len(current_quiz.keys())
+  done_questions = 0
+
   for key in current_quiz.keys():
-    options = current_quiz[key]['options']
-    counter += 1
-    print str(counter) +".", current_quiz[key]['q_text']
-    for key2 in sorted(options.keys()):
-      print   "["+ key2.upper() +"]", options[key2]
-    ans = raw_input('\nEnter your answer:')
-    print
+    while time.time() < time_allocated:
+      options = current_quiz[key]['options']
+      counter += 1
+      print str(counter) +".", current_quiz[key]['q_text']
+      for key2 in sorted(options.keys()):
+        print   "["+ key2.upper() +"]", options[key2]
+      ans = raw_input('\nEnter your answer:').lower()
+      while ans not in ['a','b', 'c', 'd']:
+        ans = raw_input('\nwrong input. Try again:').lower()
+      break
+      if ans == current_quiz[key]['is_answer']:
+        print'Correct!'
+        score += 5
+        done_questions += 1
+      elif ans != current_quiz[key]['is_answer']:
+        print 'Wrong!'
+     
+      
+  final_score = str((score/(all_questions*5))*100)
+  print "Time's Up!!"
+  time.sleep(5)
+  print 'All questions:' + str(all_questions)
+  print 'Attempted questions:' + str(done_questions)
+  print 'You scored:' + final_score  + '%'
 
             
-
-    
-
-    
-
 
 
 def main():
@@ -92,7 +110,6 @@ def main():
 
   elif option == 'quizimport':
     option = raw_input('What category would you like to import?   ')
-    # quiz_name = option
     quiz_import(option)
 
   elif option == 'quiztake':
